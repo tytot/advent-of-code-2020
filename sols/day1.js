692674c7e9fb8d780ee8e4a905780dd7c4daa944
Tyler Lin <tylerhlin@gmail.com>
2020-12-01 11:00:57 -0500

    Part 2 is now actually o(n^2)

--- sols/day1.js
+++ sols/day1.js
@@ -1,7 +1,6 @@
 const reader = require('../reader')
 
 reader.getInput('inputs/input1.txt').then((input) => {
-
     // Part 1 O(N)
     let mem = {}
     for (const val of input) {
@@ -16,15 +15,21 @@ reader.getInput('inputs/input1.txt').then((input) => {
     // Part 2 O(N^2)
     mem = {}
     for (const val1 of input) {
-        mem[val1] = input.map((val2) => val1 + val2)
+        mem[val1] = {}
+        for (const val2 of input) {
+            if (val1 !== val2) 
+                mem[val1][val1 + val2] = val2
+        }
     }
     outer: for (const val of input) {
         const other = 2020 - val
         for (const key in mem) {
-            const index = mem[key].indexOf(other)
-            if (index !== -1) {
-                const last = input[index]
-                console.log(`Solution found: ${val} * ${key} * ${last} = ${val * key * last}`)
+            if (mem[key][other]) {
+                console.log(
+                    `Solution found: ${val} * ${key} * ${mem[key][other]} = ${
+                        val * key * mem[key][other]
+                    }`
+                )
                 break outer
             }
         }
